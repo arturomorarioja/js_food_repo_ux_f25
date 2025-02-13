b9972fc4c5b271afb011a027bca0182b00e95b7c
Arturo Mora-Rioja <arturomora@gmail.com>
2025-02-13 09:39:35 +0100

    Random recipes shown on homepage

---
+++ js/recipes.js
@@ -0,0 +1,29 @@
+import { BASE_URL } from './info.js';
+
+const DEFAULT_RECIPES = 10;
+
+const showRandomRecipes = (numRecipes = DEFAULT_RECIPES) => {
+
+    for (let index = 0; index < numRecipes; index++) {
+
+        fetch(`${BASE_URL}/random.php`)
+        .then(response => response.json())
+        .then(data => {
+            data = data.meals[0];
+            document.querySelector('#recipe-list').innerHTML += `
+                <article>
+                    <header>
+                        <h2>${data.strMeal}</h2>
+                    </header>
+                    <img src="${data.strMealThumb}/preview" alt="${data.strMeal}">
+                    <div>
+                        <p class="pill">${data.strCategory}</p>
+                        <p class="pill">${data.strArea}</p>
+                    </div>
+                </article>
+            `;
+        })
+        .catch(error => console.log(error));
+    }
+};
+showRandomRecipes();
