8356c517c49b53f486fce3ce9d9121af11119675
Arturo Mora-Rioja <arturomora@gmail.com>
2025-02-13 10:38:56 +0100

    CreateElement added

--- js/recipes.js
+++ js/recipes.js
@@ -55,4 +55,48 @@ const showRandomRecipesWithInnerHTMLAndOnePageRefresh = async (numRecipes = DEFA
     }
 };
 
+const showRandomRecipes = async (numRecipes = DEFAULT_RECIPES) => {
+
+    const fragment = document.createDocumentFragment();
+    for (let index = 0; index < numRecipes; index++) {
+
+        await fetch(`${BASE_URL}/random.php`)
+        .then(response => response.json())
+        .then(data => {
+            data = data.meals[0];
+
+            const h2 = document.createElement('h2');
+            h2.innerText = data.strMeal;
+
+            const header = document.createElement('header');
+            header.append(h2);
+
+            const img = document.createElement('img');
+            img.setAttribute('src', `${data.strMealThumb}/preview`);
+            img.setAttribute('alt', data.strMeal);
+
+            const pCategory = document.createElement('p');
+            pCategory.classList.add('pill');
+            pCategory.innerText = data.strCategory;
+
+            const pArea = document.createElement('p');
+            pArea.classList.add('pill');
+            pArea.innerText = data.strArea;
+            
+            const div = document.createElement('div');
+            div.append(pCategory);
+            div.append(pArea);
+
+            const article = document.createElement('article');
+            article.append(header);
+            article.append(img);
+            article.append(div);
+
+            fragment.append(article);
+        })
+        .catch(error => console.log(error));
+    }
+    document.querySelector('#recipe-list').append(fragment);
+};
+
 showRandomRecipes();
