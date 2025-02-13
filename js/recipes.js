1bb768876261c123e0b3697900107bc4e9d5cf15
Arturo Mora-Rioja <arturomora@gmail.com>
2025-02-13 10:23:58 +0100

    InnerHTML and one page refresh added

--- js/recipes.js
+++ js/recipes.js
@@ -2,7 +2,7 @@ import { BASE_URL } from './info.js';
 
 const DEFAULT_RECIPES = 10;
 
-const showRandomRecipes = (numRecipes = DEFAULT_RECIPES) => {
+const showRandomRecipesWithInnerHTML = (numRecipes = DEFAULT_RECIPES) => {
 
     for (let index = 0; index < numRecipes; index++) {
 
@@ -26,4 +26,33 @@ const showRandomRecipes = (numRecipes = DEFAULT_RECIPES) => {
         .catch(error => console.log(error));
     }
 };
+
+const showRandomRecipesWithInnerHTMLAndOnePageRefresh = async (numRecipes = DEFAULT_RECIPES) => {
+
+    let recipeList = '';
+    for (let index = 0; index < numRecipes; index++) {
+
+        await fetch(`${BASE_URL}/random.php`)
+        .then(response => response.json())
+        .then(data => {
+            data = data.meals[0];
+            recipeList += `
+                <article>
+                    <header>
+                        <h2>${data.strMeal}</h2>
+                    </header>
+                    <img src="${data.strMealThumb}/preview" alt="${data.strMeal}">
+                    <div>
+                        <p class="pill">${data.strCategory}</p>
+                        <p class="pill">${data.strArea}</p>
+                    </div>
+                </article>
+            `;
+        })
+        .catch(error => console.log(error));
+
+        document.querySelector('#recipe-list').innerHTML = recipeList;
+    }
+};
+
 showRandomRecipes();
