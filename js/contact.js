7e615b7da37e411bc5c33cd95a56e84e6e2b1f80
Arturo Mora-Rioja <arturomora@gmail.com>
2025-02-20 12:30:31 +0100

    Contact page modal implemented

--- js/contact.js
+++ js/contact.js
@@ -5,16 +5,24 @@ document.querySelector('#frmContact').addEventListener('submit', (e) => {
     const email = e.target.txtEmail.value;
     const comments = e.target.txtComments.value;
 
-    const submittedInfo = `
-        Thank you for your message. We will get back to you shortly.
+    const modal = document.querySelector('#contact_message');
+    modal.querySelector('#name').innerText = name;
+    modal.querySelector('#email').innerText = email;
+    modal.querySelector('#comments').innerText = comments;
 
-        Registered information:
-        - Name: ${name}
-        - Email: ${email}
-        - Comments: ${comments}
-    `;
+    modal.showModal();
+    modal.focus();
 
-    alert(submittedInfo);
+    modal.querySelector('.close').addEventListener('click', function() {
+        this.parentElement.parentElement.close();
+    });
 
-    window.location.replace('index.html');
+    // Faster and less cumbersome, but sometimes you don't have the element you want to operate with
+    // modal.querySelector('.close').addEventListener('click', () => {
+    //     modal.close();
+    // });
+
+    modal.addEventListener('close', () => {
+        window.location.replace('index.html');
+    });
 });
